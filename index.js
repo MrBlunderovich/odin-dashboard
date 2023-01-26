f9fc1860f9e748a25874b57c0e88cd4f3e88a839
Maxim <334890@gmail.com>
2023-01-26 13:23:51 +1000

    Fill sidebar menu from js array

---
+++ index.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector(".sidebar__menu");
+const menuItems = [
+  { icon: "home", text: "Home" },
+  { icon: "account_box", text: "Profile" },
+  { icon: "chat_bubble", text: "Messages" },
+  { icon: "history", text: "History" },
+  { icon: "done_all", text: "Tasks" },
+  { icon: "groups_2", text: "Communities" },
+  { icon: "", text: "" },
+  { icon: "settings", text: "Settings" },
+  { icon: "help_center", text: "Support" },
+  { icon: "verified_user", text: "Privacy" },
+];
+menu.innerHTML = menuItems
+  .map(
+    (item) =>
+      `<li><i class="material-icons">${item.icon}</i>  ${item.text}</li>`
+  )
+  .join("");
